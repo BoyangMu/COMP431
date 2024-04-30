8b1025bb66ab4ad92c88c4f60d8d47e56c00b626
Boyang Mu <bmu5@unc.edu>
2024-04-30 03:02:51 -0400

    1

--- HW4/Client.py
+++ HW4/Client.py
@@ -1,63 +1,138 @@
-import sys
+import sys, socket, re
 
 
-start = 0
-end = 0
-with open(sys.argv[1],"r") as f:
-    for line in f:
-        if line[0:5] == "From:" and start == 1:
-            start = 0
-            print(".")
-            user_input = input()
-            if user_input[0:4] != "250 ":
-                print(user_input, file=sys.stderr)
-                print("QUIT")
-                exit()
-            else:
-                print(user_input, file=sys.stderr)
-            print("MAIL FROM:" + line[5:-1])
-            user_input = input()
-            if user_input[0:4] != "250 ":
-                print(user_input, file=sys.stderr)
-                print("QUIT")
-                exit()
-            else:
-                print(user_input, file=sys.stderr) 
-        elif line[0:5] == "From:":
-            print("MAIL FROM:" + line[5:-1])
-            user_input = input()
-            if user_input[0:4] != "250 ":
-                print(user_input, file=sys.stderr)
-                print("QUIT")
-                exit()
-            else:
-                print(user_input, file=sys.stderr)
-        elif line[0:3] == "To:":
-            print("RCPT TO:" + line[3:-1])
-            user_input = input()
-            if user_input[0:4] != "250 ":
-                print(user_input, file=sys.stderr)
-                print("QUIT")
-                exit()
-            else:
-                print(user_input, file=sys.stderr)
-        elif start == 0:
-            print("DATA")
-            start = 1
-            user_input = input()
-            if user_input[0:4] != "354 ":
-                print(user_input, file=sys.stderr)
-                print("QUIT")
-                exit()
-            else:
-                print(user_input, file=sys.stderr)
-            print(line[:-1])
+def next_input_line():
+    current_line = input()
+    return current_line
+
+
+def input_from():
+    #prompt for "From:" field and return input line
+    sys.stdout.write("From:\n")
+    return next_input_line().strip()
+
+def input_to():
+    #prompt for "To:" field and return input line
+    sys.stdout.write("To:\n")
+    return next_input_line().strip()
+
+def input_subject():
+    #prompt for "Subject:" field and return input
+    sys.stdout.write("Subject:\n")
+    return next_input_line().strip()
+
+
+
+def input_message():
+    sys.stdout.write("Message:\n")
+    message = ""
+    while True:
+        line = next_input_line()
+        message += line
+        if (line == ".\n" or line == '.' or line.find('.')== 0):
+            break
+    return message
+
+
+def input_email():
+    email = {"from": "", "to": "", "subject": "", "message": ""}
+    email['from'] = input_from()
+    email['to'] = input_to()
+    email['subject'] = input_subject()
+    email['message'] = input_message()
+    return email
+
+def send_data_to_server(data, socket):
+    try:
+        if(isinstance(data, list) == True):
+            socket.send(data[0].encode())
+            socket.send('.\n'.encode())
         else:
-            print(line[:-1])
-        
+            socket.send(data.encode())
+    except socket.error as err:
+        print(err)
 
-print(".")
-user_input = input()
-print(user_input, file=sys.stderr)
-print("QUIT")
-exit() 
+def receive_data_from_server(socket):
+    try:
+        message = socket.recv(4096).decode()
+        sys.stdout.write(message)
+        return message
+    except socket.error as err:
+        socket.close()
+        quit()
+
+
+def get_server_response_code(socket):
+    response = receive_data_from_server(socket)
+    response_code = ""
+    #todo: only accept printable characters (instead of .*)
+    match = re.fullmatch("(250|354|500|501|503)[ \t]+.*\n", response)
+    if match:
+        response_code = match.group(1)
+
+    return response_code
+
+
+def send_data_to_server_and_expect_response_code(data, expected_response_code, socket):
+    send_data_to_server(data,socket)
+    response_code = get_server_response_code(socket)
+    if response_code != expected_response_code:
+        quit_smtp(socket)
+        sys.exit(0)
+
+def quit_smtp(socket):
+    send_data_to_server("QUIT\n", socket)
+    receive_data_from_server(socket)
+    socket.close()
+    quit()
+
+def send_email(mail, socket):
+    send_data_to_server_and_expect_response_code(f"MAIL FROM: <{mail['from']}>\n", "250", socket)
+    send_data_to_server_and_expect_response_code(f"RCPT TO: <{mail['to']}>\n", "250", socket)
+    send_data_to_server_and_expect_response_code("DATA\n", "354", socket)
+
+    message =  f"From: <{mail['from']}>\n"
+    message += f"To: <{mail['to']}>\n"
+    message += f"Subject: {mail['subject']}\n"
+    message += f"\n"
+    message += f"{mail['message']}"
+
+    send_data_to_server_and_expect_response_code(message, "250", socket)
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+connected = False
+
+def conductGreetings(host, socket):
+    try:
+        # greeting
+        message = socket.recv(4096).decode()
+        greeting = re.match('220\s+[ -~]*', message)
+        sys.stdout.write(message)
+
+        if greeting:
+            socket.send(f"HELO {host}\n".encode())
+            # greeting going the other way
+            message = socket.recv(4096).decode()
+            okay = re.match('250\s+[ -~]*', message)
+            sys.stdout.write(message)
+            if okay:
+                return True
+        return False
+    except socket.error as err:
+        quit()
+
+connected = False
+
+while (True):
+    try:
+        if not connected:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+            conductGreetings(host, s)
+            serverSocket = s
+            connected = True
+        email = input_email()
+        send_email(email, serverSocket)
+    except EOFError:
+        quit_smtp(serverSocket)
