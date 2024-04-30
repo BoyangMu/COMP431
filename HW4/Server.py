6a5436efdfc0a2da1b07b72075951f029a0e76ab
Boyang Mu <bmu5@unc.edu>
2024-04-30 03:38:24 -0400

    1

--- HW4/Server.py
+++ HW4/Server.py
@@ -253,6 +253,7 @@ def is_crlf(user_input):
 
 connected = False
 s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostname()
 port = int(sys.argv[1])
 s.bind((host,port))
